b0f8ee540a734fdf80868970ae0bd7de5842e62d
denis <denispavlov1989@gmail.com>
2019-01-20 21:12:16 +0700

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+**Spring boot rest service**
+
+Код со стримов на youtube 
+- <a href="https://www.youtube.com/channel/UC6YiUUmiA0Gc7uXR3MZGU-w">канал</a>
+- <a href="https://t.me/JusaF">телеграм</a>
+  
---
+++ src/main/resources/db/migration/V1_0_0__init.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS item (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR (100) NOT NULL,
+  price BIGINT NOT NULL,
+  quantity INTEGER NOT NULL,
+
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS category (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR (100) NOT NULL,
+
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS item_category (
+  item_id     INTEGER REFERENCES item (id),
+  category_id INTEGER REFERENCES category (id)
+);
+
+
+--- seeds ----
+INSERT INTO item(name, quantity, price) VALUES ('Белый друг Extra Super', 5, 500000);
+INSERT INTO item(name, quantity, price) VALUES ('Черный друг Ultra Extra Super', 5, 700000);
+INSERT INTO item(name, quantity, price) VALUES ('Сразу два друга Lux', 1, 1000000);
+
+INSERT INTO item(name, quantity, price) VALUES ('Супер подружка-2000', 5, 500000);
+INSERT INTO item(name, quantity, price) VALUES ('Фронтовая часть-8000', 3, 1200000);
+INSERT INTO item(name, quantity, price) VALUES ('Бэковая часть-8000', 3, 1200000);
+
+INSERT INTO item(name, quantity, price) VALUES ('Плеть унижения', 10, 200000);
+INSERT INTO item(name, quantity, price) VALUES ('Кожаные штаны из плохого разработчика', 1, 5000000);
+
+
+INSERT INTO category(name) VALUES ('Для нее');
+INSERT INTO category(name) VALUES ('Для него');
+INSERT INTO category(name) VALUES ('БДСМ для всех');
+
+
+INSERT INTO item_category VALUES (1, 1);
+INSERT INTO item_category VALUES (2, 1);
+INSERT INTO item_category VALUES (3, 1);
+INSERT INTO item_category VALUES (4, 2);
+INSERT INTO item_category VALUES (5, 2);
+INSERT INTO item_category VALUES (6, 2);
+INSERT INTO item_category VALUES (7, 3);
+INSERT INTO item_category VALUES (8, 3);
