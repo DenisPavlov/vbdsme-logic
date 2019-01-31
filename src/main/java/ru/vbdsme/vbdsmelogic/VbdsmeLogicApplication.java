47b8f4e1547687dfd8b891a9382eef815c5792f6
DenisPavlov <denispavlov1989@gmail.com>
2019-01-31 20:23:25 +1000

    Второй стрим

--- src/main/java/ru/vbdsme/vbdsmelogic/VbdsmeLogicApplication.java
+++ src/main/java/ru/vbdsme/vbdsmelogic/VbdsmeLogicApplication.java
@@ -2,12 +2,19 @@ package ru.vbdsme.vbdsmelogic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.vbdsme.vbdsmelogic.repository.CategoryRepository;
 
 @SpringBootApplication
 public class VbdsmeLogicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VbdsmeLogicApplication.class, args);
+		ApplicationContext context = SpringApplication.run(VbdsmeLogicApplication.class, args);
+
+		CategoryRepository repository = context.getBean(CategoryRepository.class);
+
+		repository.findAll();
+		System.out.println(repository);
 	}
 
 }
